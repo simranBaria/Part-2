7211ecf572c4a71e10f46d295de7d469488a606f
simranBaria <simranbaria06@gmail.com>
2024-02-01 14:42:18 -0500

    Plane is landing

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -12,6 +12,8 @@ public class Plane : MonoBehaviour
     Rigidbody2D rigidbody;
     Vector2 currentPosition;
     public float speed = 1;
+    public AnimationCurve landing;
+    private float timerValue;
 
     private void Start()
     {
@@ -38,6 +40,17 @@ public class Plane : MonoBehaviour
 
     private void Update()
     {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            timerValue += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(timerValue);
+            if (transform.localScale.z  < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+
         lineRenderer.SetPosition(0, transform.position);
         if (points.Count > 0)
         {
