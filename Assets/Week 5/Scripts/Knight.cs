e237b33caeb4edfff54bcc65d9437f31f585dcfd
simranBaria <simranbaria06@gmail.com>
2024-02-08 15:18:46 -0500

    Task 2 complete: health bar ui displays health

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -10,12 +10,15 @@ public class Knight : MonoBehaviour
     Rigidbody2D rb;
     Animator animator;
     bool clickingOnSelf = false;
+    public float health;
+    public float maxHealth = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        health = maxHealth;
     }
 
     private void FixedUpdate()
@@ -44,11 +47,26 @@ public class Knight : MonoBehaviour
     private void OnMouseDown()
     {
         clickingOnSelf = true;
-        animator.SetTrigger("TakeDamage");
+        SendMessage("TakeDamage", 1);
     }
 
     private void OnMouseUp()
     {
         clickingOnSelf = false;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        if(health == 0)
+        {
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+        }
     }
 }
