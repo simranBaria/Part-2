1ac6c81d8a22b15ab53cdd5135b67345218f067c
simranBaria <simranbaria06@gmail.com>
2024-02-15 14:22:30 -0500

    Task 2 complete: game switches between scenes

---
+++ Assets/Week 6/Scripts/SceneName.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneName : MonoBehaviour
+{
+    TextMeshProUGUI sceneNameLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneNameLabel = GetComponent<TextMeshProUGUI>();
+        sceneNameLabel.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
